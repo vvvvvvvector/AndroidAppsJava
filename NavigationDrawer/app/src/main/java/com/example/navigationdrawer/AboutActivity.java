719573e1c0c76a20e4058e27bd630273ba74376e
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-06-03 16:17:43 +0200

    Navigation drawer test

---
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/AboutActivity.java
@@ -0,0 +1,50 @@
+package com.example.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AboutActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void onClickMenu(View view) {
+        MainActivity.openDrawer(drawerLayout);
+    }
+
+    public void onClickLogo(View view) {
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+    public void onClickHome(View view) {
+        MainActivity.redirectActivity(this, MainActivity.class);
+    }
+
+    public void onClickDashboard(View view) {
+        MainActivity.redirectActivity(this, DashboardActivity.class);
+    }
+
+    public void onClickAbout(View view) {
+        recreate();
+    }
+
+    public void onClickLogout(View view) {
+        MainActivity.logout(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MainActivity.closeDrawer(drawerLayout);
+    }
+}
---
+++ NavigationDrawer/app/src/main/java/com/example/navigationdrawer/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.navigationdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+// Viktar Zhdanovich
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void onClickMenu(View view) {
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void onClickLogo(View view) {
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void onClickHome(View view) {
+        recreate();
+    }
+
+    public void onClickDashboard(View view) {
+        redirectActivity(this, DashboardActivity.class);
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity, aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+
+    public void about(View view) {
+        redirectActivity(this, AboutActivity.class);
+    }
+
+    public void onClickLogout(View view) {
+        logout(this);
+    }
+
+    public static void logout(Activity activity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("logout");
+        builder.setMessage("are you sure?");
+        builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+}
