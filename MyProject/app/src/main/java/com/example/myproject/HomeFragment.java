68fee9c839f50b601ad730ffdf005445af12b177
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-06-04 00:13:26 +0200

    changed colors to light green; clearing edit text when click in home fragment

--- MyProject/app/src/main/java/com/example/myproject/HomeFragment.java
+++ MyProject/app/src/main/java/com/example/myproject/HomeFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class HomeFragment extends Fragment {
@@ -20,12 +21,28 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        EditText userEmail = view.findViewById(R.id.sign_in_email);
+        EditText userPassword = view.findViewById(R.id.sign_in_password);
         TextView signUpButton = view.findViewById(R.id.sign_up_text);
+
+        userEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userEmail.getText().clear();
+            }
+        });
+
+        userPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userPassword.getText().clear();
+            }
+        });
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("doc", "hello world!");
+                Log.d("doc", "hello world");
             }
         });
 
