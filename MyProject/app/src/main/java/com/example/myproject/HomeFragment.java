c5b92c8ff78cafead74195788cc01cd354d0e4bf
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-06-04 15:04:46 +0200

    sign up fragment

--- MyProject/app/src/main/java/com/example/myproject/HomeFragment.java
+++ MyProject/app/src/main/java/com/example/myproject/HomeFragment.java
@@ -1,20 +1,29 @@
 package com.example.myproject;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    OnAuthenticationListener onAuthenticationListener;
 
     public HomeFragment() {
         // Required empty public constructor
+    }
+
+    public interface OnAuthenticationListener {
+        void authenticationOperationPerformed(String method);
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,7 +32,11 @@ public class HomeFragment extends Fragment {
 
         EditText userEmail = view.findViewById(R.id.sign_in_email);
         EditText userPassword = view.findViewById(R.id.sign_in_password);
-        TextView signUpButton = view.findViewById(R.id.sign_up_text);
+        Button signInButton = view.findViewById(R.id.sign_in_button);
+        TextView signUpText = view.findViewById(R.id.sign_up_text);
+
+        signInButton.setOnClickListener(this);
+        signUpText.setOnClickListener(this);
 
         userEmail.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,13 +52,28 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("doc", "hello world");
-            }
-        });
-
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.sign_in_button) {
+            onAuthenticationListener.authenticationOperationPerformed("Sign in");
+        } else if (id == R.id.sign_up_text) {
+            onAuthenticationListener.authenticationOperationPerformed("Sign up");
+        }
+    }
+
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        Activity activity = (Activity) context;
+
+        try {
+            onAuthenticationListener = (OnAuthenticationListener) activity;
+        } catch (ClassCastException error) {
+            throw new ClassCastException(activity.toString() + " you must implement interface!");
+        }
     }
 }
--- MyProject/app/src/main/java/com/example/myproject/MainActivity.java
+++ MyProject/app/src/main/java/com/example/myproject/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnAuthenticationListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
                 .beginTransaction()
                 .add(R.id.fragment_container, homeFragment)
                 .commit();
+    }
+
+    @Override
+    public void authenticationOperationPerformed(String method) {
+        switch (method) {
+            case "Sign in":
+                Log.d("doc", "sign in operation");
+                break;
+            case "Sign up":
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new SignUpFragment())
+                        .addToBackStack(null)
+                        .commit();
+                break;
+        }
     }
 }
