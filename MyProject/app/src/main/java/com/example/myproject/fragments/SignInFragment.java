9d4d334d4e1475d53a7f66c2b6bcf5ae4a01fc10
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-06-07 11:17:35 +0200

    connected project to firebase, added sign up and sign in logic

--- MyProject/app/src/main/java/com/example/myproject/fragments/SignInFragment.java
+++ MyProject/app/src/main/java/com/example/myproject/fragments/SignInFragment.java
@@ -7,19 +7,27 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myproject.R;
 import com.example.myproject.callbackinterfaces.OnAuthenticationListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class SignInFragment extends Fragment implements View.OnClickListener {
+public class SignInFragment extends Fragment {
 
     OnAuthenticationListener onAuthenticationListener;
+
+    FirebaseAuth auth;
 
     public SignInFragment() {
         // Required empty public constructor
@@ -29,13 +37,13 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        auth = FirebaseAuth.getInstance();
+
         EditText userEmail = view.findViewById(R.id.sign_in_email);
         EditText userPassword = view.findViewById(R.id.sign_in_password);
+
         Button signInButton = view.findViewById(R.id.sign_in_button);
         TextView signUpText = view.findViewById(R.id.sign_up_text);
-
-        signInButton.setOnClickListener(this);
-        signUpText.setOnClickListener(this);
 
         userEmail.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,17 +59,38 @@ public class SignInFragment extends Fragment implements View.OnClickListener {
             }
         });
 
-        return view;
-    }
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmailString = userEmail.getText().toString();
+                String userPasswordString = userPassword.getText().toString();
 
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.sign_in_button) {
-            onAuthenticationListener.authenticationOperationPerformed("Sign in button");
-        } else if (id == R.id.sign_up_text) {
-            onAuthenticationListener.authenticationOperationPerformed("Sign up text");
-        }
+                if (TextUtils.isEmpty(userEmailString) || TextUtils.isEmpty(userPasswordString)) {
+                    Toast.makeText(getActivity(), "Empty credentials!", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.signInWithEmailAndPassword(userEmailString, userPasswordString)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        onAuthenticationListener.authenticationOperationPerformed("Sign in button");
+                                    } else {
+                                        Toast.makeText(getActivity(), "Bad credentials!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAuthenticationListener.authenticationOperationPerformed("Sign up text");
+            }
+        });
+
+        return view;
     }
 
     @Override
--- MyProject/app/src/main/java/com/example/myproject/fragments/SignUpFragment.java
+++ MyProject/app/src/main/java/com/example/myproject/fragments/SignUpFragment.java
@@ -7,21 +7,29 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.myproject.R;
 import com.example.myproject.callbackinterfaces.OnAuthenticationListener;
 import com.example.myproject.callbackinterfaces.OnBackButtonListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class SignUpFragment extends Fragment implements View.OnClickListener {
+public class SignUpFragment extends Fragment {
 
     OnAuthenticationListener onAuthenticationListener;
     OnBackButtonListener onBackButtonListener;
+
+    FirebaseAuth auth;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -32,30 +40,52 @@ public class SignUpFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
 
+        auth = FirebaseAuth.getInstance();
+
         EditText userEmail = view.findViewById(R.id.sign_up_user_email);
         EditText userPassword = view.findViewById(R.id.sign_up_password);
         EditText userConfirmPassword = view.findViewById(R.id.sign_up_confirm_password);
-        Button signUpButton = view.findViewById(R.id.sign_up_button);
 
+        Button signUpButton = view.findViewById(R.id.sign_up_button);
         LinearLayout backButton = view.findViewById(R.id.go_back_sign_up);
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 onBackButtonListener.onClickListener();
             }
         });
 
-        signUpButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmailString = userEmail.getText().toString();
+                String userPasswordString = userPassword.getText().toString();
+                String userConfirmPasswordString = userConfirmPassword.getText().toString();
+
+                if (TextUtils.isEmpty(userEmailString) || TextUtils.isEmpty(userPasswordString) || TextUtils.isEmpty(userConfirmPasswordString)) {
+                    Toast.makeText(getActivity(), "Empty credentials!", Toast.LENGTH_SHORT).show();
+                } else if (userConfirmPassword.length() < 6) {
+                    Toast.makeText(getActivity(), "Password too short!", Toast.LENGTH_SHORT).show();
+                } else if (!userPasswordString.equals(userConfirmPasswordString)) {
+                    Toast.makeText(getActivity(), "Passwords aren't equal!", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.createUserWithEmailAndPassword(userEmailString, userPasswordString)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        onAuthenticationListener.authenticationOperationPerformed("Sign up button");
+                                    } else {
+                                        Toast.makeText(getActivity(), "Error while creating account!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
 
         return view;
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.sign_up_button) {
-            onAuthenticationListener.authenticationOperationPerformed("Sign up button");
-        }
     }
 
     @Override
@@ -68,7 +98,7 @@ public class SignUpFragment extends Fragment implements View.OnClickListener {
             onAuthenticationListener = (OnAuthenticationListener) activity;
             onBackButtonListener = (OnBackButtonListener) activity;
         } catch (ClassCastException error) {
-            throw new ClassCastException(activity.toString() + " you must implement interface!");
+            throw new ClassCastException(activity + " you must implement interface!");
         }
     }
 }
